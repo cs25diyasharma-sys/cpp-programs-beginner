ac2987617c62d6833c8ec4fe11a40ff9c9ef9d1e
cs25diyasharma-sys <cs25.diyasharma@lcit.edu.in>
2026-08-22 21:15:11 +0530

    06_Average of two numbers.cpp

---
+++ 06_Average of two numbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int x,y,avg;
+clrscr();
+cout<<"Enter any number:";
+cin>>x;
+cout<<"Enter another number:";
+cin>>y;
+avg=(x+y)/2;
+cout<<"The average of two numbers is:"<<avg;
+getch();
+return 0;
