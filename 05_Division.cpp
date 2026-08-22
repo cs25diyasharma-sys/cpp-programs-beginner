0fe716dad1cdc3cf66d7f961443e5135329bf4bf
cs25diyasharma-sys <cs25.diyasharma@lcit.edu.in>
2026-08-22 21:15:51 +0530

    05_Division.cpp

--- 05_Division.cpp
+++ 05_Division.cpp
@@ -3,11 +3,12 @@
 int main()
 {
 int x,y,remainder;
+clrscr();
 cout<<"Enter a number:";
 cin>>x;
 cout<<"Enter another number:";
 cin>>y;
-remainder=x%y;
+remainder=x/y;
 cout<<"The remainder is:"<<remainder;
 getch();
 return 0;
