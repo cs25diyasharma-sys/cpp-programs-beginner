5f434b7741164ae648916eed9390d2c4b0a40418
cs25diyasharma-sys <cs25.diyasharma@lcit.edu.in>
2026-08-23 18:28:15 +0530

    07_Swaptwonumbers.cpp

---
+++ 07_Swaptwonumbers.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int x=3,y=4,temp;
+clrscr();
+cout<<"Before swapping:a="<<a<<",b="<<b<<endl;
+temp=a;
+a=b;
+b=temp;
+cout<<"After swapping:a="<<a<<",b="<<b<<endl;
+getch();
+return 0;
+}
+
+
