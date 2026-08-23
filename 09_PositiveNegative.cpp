1e2dc5ba05fcfc51bc046db07c0d48450a3332f1
cs25diyasharma-sys <cs25.diyasharma@lcit.edu.in>
2026-08-23 19:38:53 +0530

    09_PositiveNegative.cpp

--- 09_PositiveNegative.cpp
+++ 09_PositiveNegative.cpp
@@ -1,19 +1,19 @@
 #include<iostream.h>
 #include<conio.h>
-int main();
+int main()
 {
 int n;
 clrscr();
 cout<<"Enter any number:";
 cin>>n;
 if(n>=0)
- {
+  {
   cout<<"The number is positive";
- }
+  }
 else
- {
+  {
   cout<<"The number is negative";
- }
+  }
 getch();
 return 0;
 }
