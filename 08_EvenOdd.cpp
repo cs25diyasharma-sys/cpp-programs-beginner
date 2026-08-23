1c338759a1cdecd873e2788d9bce0aa42c2da0dd
cs25diyasharma-sys <cs25.diyasharma@lcit.edu.in>
2026-08-23 19:09:27 +0530

    08_EvenOdd.cpp

---
+++ 08_EvenOdd.cpp
@@ -0,0 +1,19 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int n;
+clrscr();
+cout<<"Enter any number:";
+cin>>n;
+if(n%2==0)
+ {
+ cout<<"The number is even";
+ }
+else
+ {
+ cout<<"The number is odd";
+ }
+getch();
+return 0;
+}
