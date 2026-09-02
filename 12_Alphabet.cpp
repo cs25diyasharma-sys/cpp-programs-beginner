eeeda37384b003933b00277ed4f693b8ab9d9c96
cs25diyasharma-sys <cs25.diyasharma@lcit.edu.in>
2026-09-02 14:27:44 +0530

    12_Alphabet.cpp

---
+++ 12_Alphabet.cpp
@@ -0,0 +1,35 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+clrscr();
+char n;
+cout<<"Enter any alphabet:";
+cin>>n;
+if (n=='a')
+{
+ cout<<"Alphabet is a vowel";
+}
+else if (n=='e')
+{
+cout<<"Alphabet is a vowel";
+}
+else if (n=='i')
+{
+cout<<"Alphabet is a vowel";
+}
+else if (n=='o')
+{
+cout<<"Alphabet is a vowel";
+}
+else if (n=='u')
+{
+cout<<"Alphabet is a vowel";
+}
+else
+{
+cout<<"Alphabet is a consonent";
+}
+getch();
+return 0;
+}
