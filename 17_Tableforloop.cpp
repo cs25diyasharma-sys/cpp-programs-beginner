10d25e7c383085ef32b00b79d8efa9a410395442
cs25diyasharma-sys <cs25.diyasharma@lcit.edu.in>
2026-09-20 22:03:07 +0530

    17_Tableforloop.cpp

---
+++ 17_Tableforloop.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int n,i;
+clrscr();
+cout<<"Enter number:"<<n<<endl;
+for(i=1;i<=10;i++)
+{
+cout<<n<<"x"<<i<<"="<<n*i<<endl;
+}
+getch();
+return 0;
+}
